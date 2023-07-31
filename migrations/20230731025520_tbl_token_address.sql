8f1cbe1c459c97db50379bcea2cd490454132ce0
Ayoung <saing.sab@gmail.com>
2023-07-31 10:10:35 +0700

    finish table creation

---
+++ migrations/20230731025520_tbl_token_address.sql
@@ -0,0 +1,14 @@
+-- Add migration script here
+CREATE TABLE
+    IF NOT EXISTS tbl_token_address (
+        id UUID PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
+        token_address VARCHAR(64) NOT NULL,
+        token_symbol VARCHAR(10) NOT NULL,
+        created_by UUID,
+        created_at TIMESTAMP
+        WITH
+            TIME ZONE DEFAULT NOW(),
+            updated_at TIMESTAMP
+        WITH
+            TIME ZONE DEFAULT NOW()
+    );
