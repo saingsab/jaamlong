58f0c11e157351014c6d9b7cf534538ce1d321f8
Sakal05 <samnang.sakal@gmail.com>
2023-08-03 17:24:38 +0700

    undo comment psql checking

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,10 +2,10 @@
 set -x
 set -eo pipefail
 
-# if ! [ -x "$(command -v psql)" ]; then
-#   echo >&2 "Error: psql is not installed."
-#   exit 1
-# fi
+if ! [ -x "$(command -v psql)" ]; then
+  echo >&2 "Error: psql is not installed."
+  exit 1
+fi
 
 if ! [ -x "$(command -v sqlx)" ]; then
   echo >&2 "Error: sqlx is not installed."
