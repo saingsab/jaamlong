eafbebc2cbf6fa0f960df2e7d5cacc82696254b2
Sakal05 <samnang.sakal@gmail.com>
2023-09-04 11:36:09 +0700

    add abi json

--- migrations/20230731025520_tbl_token_address.sql
+++ migrations/20230731025520_tbl_token_address.sql
@@ -4,6 +4,7 @@ CREATE TABLE
         id UUID PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
         token_address VARCHAR(64) NOT NULL,
         token_symbol VARCHAR(10) NOT NULL,
+        abi JSONB,
         created_by UUID,
         created_at TIMESTAMP
         WITH
