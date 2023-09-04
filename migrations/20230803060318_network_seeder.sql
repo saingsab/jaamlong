0c406e05d4e02729c543a3c53e2e459aba0130a2
Sakal05 <samnang.sakal@gmail.com>
2023-09-04 11:36:37 +0700

    update network seeder

--- migrations/20230803060318_network_seeder.sql
+++ migrations/20230803060318_network_seeder.sql
@@ -1,7 +1,7 @@
 -- Add migration script here
-INSERT INTO tbl_networks (network_name, network_rpc, created_by)
+INSERT INTO tbl_networks (network_name, network_rpc, chain_id, decimal_value, created_by)
 VALUES
-    ('Goerli','https://eth-goerli.public.blastapi.io', '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0'),
-    ('Binance','https://data-seed-prebsc-1-s1.binance.org:8545/', '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0'),
-    ('Fuji', 'https://ava-testnet.public.blastapi.io/ext/bc/C/rpc', '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0'),
-    ('Polygon','https://polygon-testnet-rpc.allthatnode.com:8545', '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0');
+    ('Goerli','https://eth-goerli.public.blastapi.io', 5, 18, '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0'),
+    ('Binance','https://data-seed-prebsc-1-s1.binance.org:8545/', 97, 18, '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0'),
+    ('Fuji', 'https://ava-testnet.public.blastapi.io/ext/bc/C/rpc', 43113, 18, '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0'),
+    ('Polygon','https://polygon-testnet-rpc.allthatnode.com:8545', 80001, 18, '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0');
