b5926cd655b9ec88a9ea3908da82d8024ab17817
Sakal05 <samnang.sakal@gmail.com>
2023-09-11 13:43:22 +0700

    modify the name of request

--- src/routers/transaction_routes.rs
+++ src/routers/transaction_routes.rs
@@ -1,16 +1,16 @@
-use crate::handlers::transaction_handler::{broadcast_tx, get_all_tx, validate_tx};
+use crate::handlers::transaction_handler::{broadcast_tx, get_all_tx, request_tx};
 use crate::AppState;
 use axum::{
-    routing::{get, post},
+    routing::post,
     Router,
 };
 use std::sync::Arc;
 
 pub fn transaction_routes(app_state: Arc<AppState>) -> Router {
     Router::new()
-        .route("/", get(get_all_tx))
-        .route("/validate_tx", post(validate_tx))
-        .route("/broadcast_tx", post(broadcast_tx))
+        // .route("/", get(get_all_tx))
+        .route("/request-tx", post(request_tx))
+        .route("/broadcast-tx", post(broadcast_tx))
         // .route("/api/notes", get(note_list_handler))
         // .route(
         //     "/api/notes/:id",
