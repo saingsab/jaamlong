bb0c1d831c62694bcddb8fee6fc02f9fb923b45f
Sakal05 <samnang.sakal@gmail.com>
2023-09-18 11:14:35 +0700

    remove bridge key query

--- migrations/20230803060318_network_seeder.sql
+++ migrations/20230803060318_network_seeder.sql
@@ -1,7 +1,7 @@
 -- Add migration script here
-INSERT INTO tbl_networks (network_name, network_rpc, chain_id, decimal_value, created_by)
+INSERT INTO tbl_networks (network_name, network_rpc, bridge_address, bridge_fee, chain_id, decimal_value, created_by)
 VALUES
-    ('Goerli','https://eth-goerli.public.blastapi.io', 5, 18, '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0'),
-    ('Binance','https://data-seed-prebsc-1-s1.binance.org:8545/', 97, 18, '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0'),
-    ('Fuji', 'https://ava-testnet.public.blastapi.io/ext/bc/C/rpc', 43113, 18, '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0'),
-    ('Polygon','https://polygon-testnet-rpc.allthatnode.com:8545', 80001, 18, '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0');
+    ('Goerli','https://eth-goerli.public.blastapi.io', '0xCF6F0d155989B11Ba3882e99c72f609f0C06e086', 0.005, 5, 18, '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0'),
+    ('Binance','https://data-seed-prebsc-1-s1.binance.org:8545/', '0xCF6F0d155989B11Ba3882e99c72f609f0C06e086', 0.005, 97, 18, '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0'),
+    ('Fuji', 'https://ava-testnet.public.blastapi.io/ext/bc/C/rpc', '0xCF6F0d155989B11Ba3882e99c72f609f0C06e086', 0.005, 43113, 18, '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0'),
+    ('Polygon','https://polygon-testnet-rpc.allthatnode.com:8545', '0xCF6F0d155989B11Ba3882e99c72f609f0C06e086', 0.005, 80001, 18, '4e4adca8-6c6b-4a3f-92c2-8be6d4571ed0');
