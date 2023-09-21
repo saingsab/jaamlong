8788d26e557e0ec2c7c6fd2f8bdb80444ae1a165
Sakal05 <samnang.sakal@gmail.com>
2023-09-22 00:05:54 +0700

    add add_token handler

--- src/handlers/token_address_handler.rs
+++ src/handlers/token_address_handler.rs
@@ -5,7 +5,8 @@ use axum::{
     response::IntoResponse,
     Json,
 };
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
 use std::sync::Arc;
 use uuid::Uuid;
 
@@ -14,6 +15,15 @@ pub struct ResponseToken {
     pub id: Uuid,
     pub token_address: String,
     pub token_symbol: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RequestToken {
+    pub token_address: String,
+    pub token_symbol: String,
+    pub asset_type: String,
+    pub abi: Option<sqlx::types::JsonValue>,
+    pub created_by: Uuid,
 }
 
 pub async fn get_all_token_addresses(
@@ -61,6 +71,69 @@ pub async fn get_token_address_by_id(
             let json_response = serde_json::json!({
                 "status": "success",
                 "data": token
+            });
+            Ok(Json(json_response))
+        }
+        Err(err) => {
+            let json_response = serde_json::json!({
+                "status": "fail",
+                "data": format!("Error: {}", err)
+            });
+            Ok(Json(json_response))
+        }
+    }
+}
+
+pub async fn add_token(
+    State(data): State<Arc<AppState>>,
+    Json(payload): Json<RequestToken>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let insert = sqlx::query!(
+        r#"
+            INSERT INTO tbl_token_address (token_address, token_symbol, asset_type, created_by)
+            VALUES
+                ($1, $2, $3,$4);  
+        "#,
+        payload.token_address,
+        payload.token_symbol,
+        payload.asset_type,
+        payload.created_by,
+    )
+    .execute(&data.db)
+    .await;
+    match insert {
+        Ok(insert) => {
+            println!("Successfully inserted! Result: {:#?}", insert);
+        }
+        Err(err) => {
+            let json_response = serde_json::json!({
+                "status": "fail",
+                "data": format!("Error: {}", err)
+            });
+            return Ok(Json(json_response));
+        }
+    }
+    #[derive(Debug, FromRow, Serialize)]
+    pub struct Response {
+        id: Uuid,
+        token_address: String,
+        token_symbol: String,
+        asset_type: String,
+        created_by: Option<Uuid>,
+    }
+    let insert_result = sqlx::query_as!(
+        Response,
+        r#"
+            SELECT id, token_address, token_symbol, asset_type, created_by FROM tbl_token_address
+        "#
+    )
+    .fetch_one(&data.db)
+    .await;
+    match insert_result {
+        Ok(result) => {
+            let json_response = serde_json::json!({
+                "status": "success",
+                "data": result
             });
             Ok(Json(json_response))
         }
