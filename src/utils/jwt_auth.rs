74c44368108ef4e3a6570de11245e1e1b19b8371
Sakal05 <samnang.sakal@gmail.com>
2023-10-06 01:09:40 +0700

    update format of extracting bearer token

--- src/utils/jwt_auth.rs
+++ src/utils/jwt_auth.rs
@@ -43,11 +43,9 @@ pub async fn auth<B>(
                 .get(header::AUTHORIZATION)
                 .and_then(|auth_header| auth_header.to_str().ok())
                 .and_then(|auth_value| {
-                    if auth_value.starts_with("Bearer ") {
-                        Some(auth_value[7..].to_owned())
-                    } else {
-                        None
-                    }
+                    auth_value
+                        .strip_prefix("Bearer ")
+                        .map(|bearer| bearer.to_owned())
                 })
         });
 
